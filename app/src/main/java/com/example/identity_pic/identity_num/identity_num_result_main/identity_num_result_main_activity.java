09e35039a3cb6e1bed79666ebb32b27fb2aaacc3
yubowengod <heiheijun@hotmail.com>
2016-11-26 13:58:52 +0800

    11-26 southcar 基本界面初稿

--- app/src/main/java/com/example/identity_pic/identity_num/identity_num_result_main/identity_num_result_main_activity.java
+++ app/src/main/java/com/example/identity_pic/identity_num/identity_num_result_main/identity_num_result_main_activity.java
@@ -1,12 +1,17 @@
 package com.example.identity_pic.identity_num.identity_num_result_main;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.GridView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.example.god.southcar.R;
 import com.example.identity_pic.identity_num.identity_1_activity;
+import com.example.identity_pic.identity_num.identity_num_listview_main.identity_num_listview_main_activity1;
 
 /**
  * Created by GOD on 2016/10/25.
@@ -20,5 +25,24 @@ public class identity_num_result_main_activity extends AppCompatActivity {
         identity_num_result_main_view.setAdapter(new identity_num_result_main_activity_adapter(
                 identity_num_result_main_activity.this,
                 identity_1_activity.no1,identity_1_activity.no1_weizhi));
+
+        Button identity_num_result_main_btn_right = (Button) findViewById(R.id.identity_num_result_main_btn_right);
+        Button identity_num_result_main_btn_left = (Button) findViewById(R.id.identity_num_result_main_btn_left);
+
+        identity_num_result_main_btn_right.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(identity_num_result_main_activity.this, "fuck you!", Toast.LENGTH_SHORT).show();
+            }
+        });
+        identity_num_result_main_btn_left.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                identity_1_activity.no1 = new String[4];
+                Intent intent = new Intent(identity_num_result_main_activity.this,identity_num_listview_main_activity1.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
