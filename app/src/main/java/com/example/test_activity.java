eed52e242720217b5046dc0348f8a8e26e40c8d5
yubowengod <heiheijun@hotmail.com>
2016-11-29 00:27:44 +0800

    11-26 southcar 基本界面初稿
    上传图片deal
    调用dll 不好用。。。。。
    调用dll 初步搞定，后续 to do
    调用dll deal
    lib引用相册路径重新写--
    test_activity 1234 deal

--- app/src/main/java/com/example/test_activity.java
+++ app/src/main/java/com/example/test_activity.java
@@ -41,14 +41,15 @@ public class test_activity extends AppCompatActivity {
     private static final int REQUEST_CODE = 0;
     private TextView tvResult;
     private TextView tvResult1;
+    private TextView tvResult2;
+    private TextView tvResult3;
     private EditText editText;
 
     private SimpleDraweeView draweeView;
     ArrayList<String> pic_path=new ArrayList<>();
     ArrayList<String> pic_flag=new ArrayList<>();
     ArrayList<String> pic_path_test=new ArrayList<>();
-//     String  pic_path_flag;
-//     String  pic_path_test;
+
     private ExecutorService executorService;
     private Handler mainHandler = new Handler() {
         @Override
@@ -70,6 +71,9 @@ public class test_activity extends AppCompatActivity {
 
         tvResult = (TextView) findViewById(R.id.tvResult);
         tvResult1 = (TextView) findViewById(R.id.tvResult1);
+        tvResult2 = (TextView) findViewById(R.id.tvResult2);
+        tvResult3 = (TextView) findViewById(R.id.tvResult3);
+
         editText = (EditText) findViewById(R.id.test_edittext);
         draweeView = (SimpleDraweeView) findViewById(R.id.my_image_view);
     }
@@ -79,59 +83,6 @@ public class test_activity extends AppCompatActivity {
             Glide.with(context).load(path).into(imageView);
         }
     };
-
-    public void Multiselect(View view) {
-        tvResult.setText("");
-        ImgSelConfig config = new ImgSelConfig.Builder(loader).multiSelect(true)
-                // 使用沉浸式状态栏
-                .statusBarColor(Color.parseColor("#3F51B5")).build();
-
-        ImgSelActivity.startActivity(this, config, REQUEST_CODE);
-    }
-
-    public void upload(View view){
-
-        final ProgressDialog dialog = ProgressDialog.show(test_activity.this, "数据上传中", "请稍候...", true);
-
-        if (pic_path.size()>0){
-            executorService.execute(new Runnable() {
-                @Override
-                public void run() {
-//                    test_mul.getImageromSdk(pic_path);
-//                    pic_path.clear();
-                    String ww = editText.getText().toString();
-                    pic_flag.add(ww);
-//                    pic_path_test.add("D:\\web\\WebApplication1\\webnnn\\6.jpg");
-                    gap_upload_identity_result.getImageromSdk(pic_flag,pic_path);
-
-                        mainHandler.post(new Runnable() {
-                            @Override
-                            public void run() {
-
-                                if (gap_upload_identity_result.return_true_flag.size()!=0)
-                                {
-                                    dialog.dismiss();
-                                    String[] aa = gap_upload_identity_result.return_true_flag.get(0).split("@");
-                                    tvResult.setText(aa[0]);
-                                    tvResult1.setText(aa[1]);
-                                    gap_upload_identity_result.return_true_flag.clear();
-                                    pic_flag.clear();
-                                    pic_path.clear();
-                                }
-
-                            }
-                        });
-
-                }
-            });
-        }
-        else{
-            Toast.makeText(test_activity.this, "!!!", Toast.LENGTH_SHORT).show();
-        }
-
-    }
-
-
     public void Single(View view) {
         tvResult.setText("");
         ImgSelConfig config = new ImgSelConfig.Builder(loader)
@@ -158,16 +109,82 @@ public class test_activity extends AppCompatActivity {
 
         ImgSelActivity.startActivity(this, config, REQUEST_CODE);
     }
+    public void Multiselect(View view) {
+        tvResult.setText("");
+        ImgSelConfig config = new ImgSelConfig.Builder(loader).multiSelect(true)
+                // 使用沉浸式状态栏
+                .statusBarColor(Color.parseColor("#3F51B5")).build();
+
+        ImgSelActivity.startActivity(this, config, REQUEST_CODE);
+    }
+
+    public void upload(View view){
+
+        final ProgressDialog dialog = ProgressDialog.show(test_activity.this, "数据上传中", "请稍候...", true);
+
+        if (pic_path_test.size()>0){
+            executorService.execute(new Runnable() {
+                @Override
+                public void run() {
+
+//                    String ww = editText.getText().toString();
+                    String[] www =editText.getText().toString().split("@");
+                    pic_flag.add(www[0]);
+                    pic_flag.add(www[1]);
+                    pic_flag.add(www[2]);
+                    pic_flag.add(www[3]);
+//                    pic_path_test.add("D:\\web\\WebApplication1\\webnnn\\6.jpg");
+                    gap_upload_identity_result.getImageromSdk(pic_flag,pic_path_test);
+
+                        mainHandler.post(new Runnable() {
+                            @Override
+                            public void run() {
+
+                                if (gap_upload_identity_result.return_true_flag.size()==4)
+                                {
+                                    dialog.dismiss();
+                                    String[] a = gap_upload_identity_result.return_true_flag.get(0).split("@");
+                                    tvResult.setText(a[1]);
+                                    String[] aa = gap_upload_identity_result.return_true_flag.get(1).split("@");
+                                    tvResult1.setText(aa[1]);
+                                    String[] aaa = gap_upload_identity_result.return_true_flag.get(2).split("@");
+                                    tvResult2.setText(aaa[1]);
+                                    String[] aaaa = gap_upload_identity_result.return_true_flag.get(3).split("@");
+                                    tvResult3.setText(aaaa[1]);
+                                    gap_upload_identity_result.return_true_flag.clear();
+                                    pic_flag.clear();
+                                    pic_path_test.clear();
+                                }
+
+                            }
+                        });
+
+                }
+            });
+        }
+        else{
+            Toast.makeText(test_activity.this, "!!!", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == REQUEST_CODE && resultCode == RESULT_OK && data != null) {
-            List<String> pathList = data.getStringArrayListExtra(ImgSelActivity.INTENT_RESULT);
-            pic_path.add(pathList.get(0));
-            // 测试Fresco。可不理会
-            // draweeView.setImageURI(Uri.parse("file://"+pathList.get(0)));
-            for (String path : pathList) {
+//            List<String> pathList = data.getStringArrayListExtra(ImgSelActivity.INTENT_RESULT);
+//            pic_path.add(pathList.get(0));
+//            // 测试Fresco。可不理会
+//            // draweeView.setImageURI(Uri.parse("file://"+pathList.get(0)));
+//            for (String path : pathList) {
+//                tvResult.append(path + "\n");
+//            }
+
+            pic_path_test = data.getStringArrayListExtra(ImgSelActivity.INTENT_RESULT);
+            for (String path : pic_path_test) {
                 tvResult.append(path + "\n");
             }
         }
