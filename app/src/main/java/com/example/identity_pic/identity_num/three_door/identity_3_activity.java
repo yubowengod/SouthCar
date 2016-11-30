1fa0b4d5baef065d3f681ab17f7b19dd16954317
yubowengod <heiheijun@hotmail.com>
2016-11-30 15:32:12 +0800

    11-26   southcar   基本界面初稿
      上传图片deal
      调用dll 不好用。。。。。
      调用dll 初步搞定，后续 to do
      调用dll deal
      lib引用相册路径重新写--
      test_activity 1234 deal
    
      11-29
      第一种门 左上右上右下左下 deal
      备份
      第二种门 左上右上左边右边 deal
      备份
      11-30

--- app/src/main/java/com/example/identity_pic/identity_num/three_door/identity_3_activity.java
+++ app/src/main/java/com/example/identity_pic/identity_num/three_door/identity_3_activity.java
@@ -1,16 +1,35 @@
 package com.example.identity_pic.identity_num.three_door;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.god.southcar.R;
+import com.example.identity_pic.identity_num.two_door.identity_num_listview_main.identity_2_listview_main_activity1;
 
 /**
  * Created by GOD on 2016/10/20.
  */
 public class identity_3_activity extends AppCompatActivity {
+
+    public static String [] no3 = new String[4];
+    public static String [] no3_weizhi = {"左","中","右"};
+    public static String [] no3_weizhi_flag = {"7","7","7"};
+
+    private Button identity_3_activity_btn;
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.identity_3);
+        identity_3_activity_btn = (Button) findViewById(R.id.btn_identity_2_bottom);
+        identity_3_activity_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(identity_3_activity.this,identity_2_listview_main_activity1.class);
+                startActivity(intent);
+            }
+        });
     }
 }
